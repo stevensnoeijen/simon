d9b65869614b753461171ae7a24ce001966196f4
stevensnoeijen <stevensnoeijen@gmail.com>
2020-04-14 20:04:48 +0200

    user clicks implemented added button animation added opacity in button click fixed to restart a game fixed multiple clicks

--- simon/src/App.js
+++ simon/src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 export class App extends React.Component {
 
   pattern = [];
+  userStep = -1;
+  userTurn = false;
 
   constructor(props){
     super(props);
@@ -17,23 +19,25 @@ export class App extends React.Component {
     };
 
     this.startGame = this.startGame.bind(this);
+    this.onButtonClick = this.onButtonClick.bind(this);
   };
 
   render(){
     return (
       <div className="App">
         <header className="App-header">
-          <h1>Simon saids</h1>
+          <h1>Simon says</h1>
         </header>
         <div className="App-body">
           <div>
-            <div className={'button ' + (this.state.green ? "on" : "off")} style={{backgroundColor: 'green'}}></div>
-            <div className={'button ' + (this.state.red ? "on" : "off")} style={{backgroundColor: 'red'}}></div>
+            <div className={'button ' + (this.state.green ? "on" : "off")} style={{backgroundColor: 'green'}} onClick={() => this.onButtonClick('green')}></div>
+            <div className={'button ' + (this.state.red ? "on" : "off")} style={{backgroundColor: 'red'}} onClick={() => this.onButtonClick('red')}></div>
             <br/>
-            <div className={'button ' + (this.state.yellow ? "on" : "off")} style={{backgroundColor: 'yellow'}}></div>
-            <div className={'button ' + (this.state.blue ? "on" : "off")} style={{backgroundColor: 'blue'}}></div>
+            <div className={'button ' + (this.state.yellow ? "on" : "off")} style={{backgroundColor: 'yellow'}} onClick={() => this.onButtonClick('yellow')}></div>
+            <div className={'button ' + (this.state.blue ? "on" : "off")} style={{backgroundColor: 'blue'}} onClick={() => this.onButtonClick('blue')}></div>
           </div>
 
+          <br/>
           <button disabled={!this.state.startButtonEnabled} onClick={this.startGame} >Start</button>
         </div>
       </div>
@@ -41,10 +45,17 @@ export class App extends React.Component {
   };
 
   async startGame(){
+    this.pattern = [];
+    this.userStep = -1;
     this.setState({
       startButtonEnabled: false
     });
+    await this.runNewRound();
+  }
 
+  async runNewRound(){
+    console.log('level ' + (this.pattern.length + 1));
+    this.userStep = -1;
     // generate color
     const newNumber = Math.floor(Math.random() * 4);
     let color;
@@ -77,10 +88,36 @@ export class App extends React.Component {
       await sleep(250);
     }
 
-    this.setState({
-      startButtonEnabled: true
-    });
+    this.userTurn = true;
   };
+
+  onButtonClick(color){
+    if(this.userTurn === false){
+      return;
+    }
+    this.userTurn = false;
+    this.userStep++;
+
+    // check step
+    const isValid = this.pattern[this.userStep] === color;
+    console.log(`click was ${isValid}`);
+    if(!isValid) {
+      alert('incorrect! game over');
+      this.setState({
+        startButtonEnabled: true
+      });
+      return;
+    }
+    // if its the last one
+    if(this.pattern.length === (this.userStep + 1)){
+      console.log('last step clicked, start new round');
+      sleep(1000).then(() => {
+        this.runNewRound();
+      });
+    }
+
+    this.userTurn = true;
+  }
 };
 
 async function sleep(ms) {
